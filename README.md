87ab3718c20fae9739e751b8fd503462405e7896
arpapp <trpapp@gmail.com>
2020-03-20 17:25:11 -0600

    can make dumb change

---
+++ README.md
@@ -0,0 +1,10 @@
+## Make Change Project - Week 1 Skill Distillery
+
+### Overview
+
+### Technologies Used
+- Eclipse
+- Java
+- GitHub
+
+### Lessons Learned
--- src/makechange/MakeChangeApp.java
+++ src/makechange/MakeChangeApp.java
@@ -2,9 +2,32 @@ package makechange;
 
 public class MakeChangeApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+static java.util.Scanner kb = new java.util.Scanner(System.in);
 
+	public static void main(String[] args) {
+		double price, payment;
+
+		System.out.println("Enter the total price: ");
+		price = kb.nextDouble();
+		System.out.println("Enter your payment: ");
+		payment = kb.nextDouble();
+
+		if (price == payment) {
+			System.out.println(
+					"Thank you for the exact change. Where were you when I was working Christmas at Toys R Us?");
+		} else if (payment < price) {
+			System.out.println("I'm sorry, that's not enough.");
+		} else {
+			System.out.println(makeChange(price, payment));
+		}
+
+	}
+	
+	public static double makeChange(double price, double payment) {
+		double change;
+		
+		change = price - payment;
+		return change;
 	}
 
 }
